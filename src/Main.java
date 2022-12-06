65bc0299c0434ab1e024d2689cfa0058d7d5ed04
10mart10 <martspil28@gmail.com>
2022-12-06 20:39:34 +0100

    added hello world

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("This will be uno");
     }
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
 }
