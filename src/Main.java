3ba4ed7f4def7f4a24e3374c701feae425ebddde
Sem de Jong <semdejo2003@gmail.com>
2022-12-20 17:01:26 +0100

    Start on game logic

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("This will be uno stub implementation");
+        //This will be the starting point of loading the game.
+
     }
+
 }
